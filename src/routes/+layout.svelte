57768f415004a5ea15159eb0d57aff34dee9826d
JennaBerndt <riab560@googlemail.com>
2024-07-11 19:52:11 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Header from "$lib/components/Header.svelte";
+	import Footer from "$lib/components/Footer.svelte";
+	import '../app.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<slot />
+
+	<Footer />
+
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+</style>
